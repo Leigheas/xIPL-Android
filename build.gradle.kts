a55357163caf15ea39032ae39745ba4dfa97a8dd
Leigheas <63628116+Leigheas@users.noreply.github.com>
2026-04-22 23:59:47 -0500

    Add initial build.gradle.kts configuration

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+plugins {
+    id("com.android.application") version "7.0.0" apply false
+    id("kotlin-android") version "1.5.31" apply false
+}
